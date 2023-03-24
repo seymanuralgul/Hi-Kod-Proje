09e34dbff77b3568789ca1dbfee709148882b4a4
Cmrnl1 <127992147+Cmrnl1@users.noreply.github.com>
2023-03-24 11:39:08 +0300

    çiçek sayacı eklendi

--- Assets/Scripts/cicek.cs
+++ Assets/Scripts/cicek.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cicek : MonoBehaviour
 {
+    private int texticicek = 0;
+    [SerializeField] private Text cicektext;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("cicek"))
         {
             Destroy(collision.gameObject);
+            texticicek++;
+            cicektext.text="Çiçek: " + texticicek;
         }
     }
 }
